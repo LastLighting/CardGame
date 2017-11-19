9300ce1521294262d304cd359d25a36880a1164d
alex <aalleekkss55@yandex.ru>
2017-11-19 13:29:23 +0300

    Перебазирование проекта

---
+++ CardGame/Assets/Scripts/Collection/Collection.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.SceneManagement;
+
+public class Collection : MonoBehaviour {
+
+    public Card card;
+    public float startTransformX;
+    public float startTransformY;
+    public float transformX;
+    public float transformY;
+
+    void Start () {
+        Vector3 position = transform.position;
+        position.y = position.y + startTransformY;
+        position.x = position.x - startTransformX;
+        position.z = 0;
+        for (int y = 0; y < 2; y++)
+        {
+            for (int x = 0; x < 4; x++)
+            {
+                Card newCard = Instantiate(card, position, card.transform.rotation) as Card;
+                newCard.changeSprite(Random.Range(0,3));
+                position.x = position.x + transformX;
+            }
+            position.x = transform.position.x - startTransformX;
+            position.y = position.y - transformY;
+        }
+        StartCoroutine(GetText());
+
+    }
+
+    IEnumerator GetText()
+    {
+        WWWForm form = new WWWForm();
+        string jsonStr = "{\"objects\":[{\"id\":1,\"title\":\"Book\",\"position_x\":0,\"position_y\":0,\"position_z\":0,\"rotation_x\":0,\"rotation_y\":0,\"rotation_z\":0,\"created\":\"2016-09-21T14:22:22.817Z\"},{\"id\":2,\"title\":\"Apple\",\"position_x\":0,\"position_y\":0,\"position_z\":0,\"rotation_x\":0,\"rotation_y\":0,\"rotation_z\":0,\"created\":\"2016-09-21T14:22:52.368Z\"}]}";
+        form.AddField("field", "pisos");
+        form.AddField("guid", "22345200-abe8-4f60-90c8-0d43c5f6c0f6");
+        form.AddField("card", "alien");
+        var formData = System.Text.Encoding.UTF8.GetBytes(jsonStr);
+        UnityWebRequest www = UnityWebRequest.Post("https://cardgamejavaserver.herokuapp.com/user/registration", form);
+        yield return www.SendWebRequest();
+
+        if (www.isNetworkError)
+        {
+            Debug.Log(www.error);
+        }
+       
+    }
+
+}
---
+++ CardGame/Assets/Scripts/MainMenu/Menu.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour {
+
+	public GameObject playEffect;
+	public GameObject collectionEffect;
+	public GameObject optionsEffect;
+	public GameObject settingsButton;
+	public GameObject playButton;
+	public GameObject collectionButton;
+	public GameObject optionsBackground;
+	public GameObject settingsBackgroundEffect;
+    public Transform transform1;
+    public Transform transform2;
+    public Transform transform3;
+    public Transform transform4;
+    public float settingsSpeed;
+    float startTime;
+    bool open = false;
+	bool close = false;
+	bool opening = false;
+    Vector3 defaultVector1;
+    Vector3 defaultVector2;
+
+    void Start()
+    {
+        defaultVector1 = transform1.position;
+        defaultVector2 = transform3.position;
+    }
+
+    public void play() {
+		playEffect.SetActive(true);
+	}
+
+	public void collection() {
+		collectionEffect.SetActive(true);
+        SceneManager.LoadScene(0);
+    }
+
+	public void options() {
+		if (!opening) {
+            opening = true;
+            optionsEffect.SetActive (true);
+			playButton.SetActive (false);
+			collectionButton.SetActive (false);
+            startTime = Time.timeSinceLevelLoad;
+            close = false;
+			open = true;
+        } else {
+            opening = false;
+            optionsEffect.SetActive (false);
+			playButton.SetActive (true);
+			collectionButton.SetActive (true);
+			optionsBackground.SetActive(false);
+			settingsBackgroundEffect.SetActive (false);
+            startTime = Time.timeSinceLevelLoad;
+            open = false;
+			close = true;
+			
+		}
+	}
+
+	void Update() {
+        if (settingsButton.transform.position == transform2.position)
+        {
+            if (opening)
+            {
+                settingsBackgroundEffect.SetActive(true);
+                optionsBackground.SetActive(true);
+            }
+            open = false;
+        }
+        if (settingsButton.transform.position == defaultVector1)
+        {
+            close = false;
+        }
+        if (open)
+        {
+            settingsButton.transform.position = Vector3.Lerp(transform1.position, transform2.position, (Time.timeSinceLevelLoad - startTime) * settingsSpeed);
+            optionsEffect.transform.position = Vector3.Lerp(transform3.position, transform4.position, (Time.timeSinceLevelLoad - startTime) * settingsSpeed);
+        }
+        if (close)
+        {
+            settingsButton.transform.position = Vector3.Lerp(transform1.position, defaultVector1, (Time.timeSinceLevelLoad - startTime) * settingsSpeed);
+            optionsEffect.transform.position = Vector3.Lerp(transform3.position, defaultVector2, (Time.timeSinceLevelLoad - startTime) * settingsSpeed);
+        }
+        
+    }
+}
